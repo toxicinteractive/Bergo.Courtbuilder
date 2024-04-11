30659d72a6bb193b9963d43f61c1ce1dcea5b0d0
Adam Kronquist <adam.kronquist@toxic.se>
2024-04-11 12:13:43 +0200

    Fix code to send mail

--- bergocourtbuilder.com/public_html/conf/connecting.php
+++ bergocourtbuilder.com/public_html/conf/connecting.php
@@ -1,17 +1,37 @@
-<?php
-
-	$host = 'mysql579.loopia.se';
-	$db   = 'bergocourtbuilder_com';
-	$user = 'bergo@b181598';
-	$pass = 'Database4Design!';
-	$charset = 'utf8mb4';
-
-	$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-	$opt = [
-		PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-		PDO::ATTR_EMULATE_PREPARES   => false,
-	];
-	$pdo = new PDO($dsn, $user, $pass, $opt);
-	
+<?php
+
+
+
+	$host = 'mysql579.loopia.se';
+
+	$db   = 'bergocourtbuilder_com';
+
+	# For live
+	#$user = 'bergo@b181598';
+
+	# For local testing
+	$user = 'bergophp@b181598';
+
+	$pass = 'Database4Design!';
+
+	$charset = 'utf8mb4';
+
+
+
+	$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+	$opt = [
+
+		PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+
+		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+
+		PDO::ATTR_EMULATE_PREPARES   => false,
+
+	];
+
+	$pdo = new PDO($dsn, $user, $pass, $opt);
+
+	
+
 ?>
